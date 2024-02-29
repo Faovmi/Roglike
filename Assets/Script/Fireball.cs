82c860a6d9649d42423811bfd771ed29447234e0
Faovmi <159125808+Faovmi@users.noreply.github.com>
2024-02-29 21:43:02 +0700

    HM

---
+++ Assets/Script/Fireball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float Speed;
+    public float lifetime;
+    public float damage = 100;
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * Speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+
+    private void DamageEnemy(Collision collision)
+
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.value -= damage;
+            if(enemyHealth.value <= 0)
+            {
+                Destroy(enemyHealth.gameObject);
+            }
+        }
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+}
+
+
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
+
+    private Vector3 _moveVectore;
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _moveVectore = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVectore += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVectore -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVectore += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVectore -= transform.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+
+    }
+    // Update is called once per frame
+    void  FixedUpdate()
+    {
+        _characterController.Move(_moveVectore * speed * Time.fixedDeltaTime);
+
+        _fallVelocity += gravity * Time.deltaTime;
+        GetComponent<CharacterController>().Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
